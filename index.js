623e5ceb5a4377596c556d1fa223fac0195d3eac
qwazwsx <azimbin@gmail.com>
2021-10-18 13:33:01 -0500

    added 'error' event, changed style
    added 'error' event for errors thrown while logging in
    
    changed js style
    
    updated docs to reflect new event

--- index.js
+++ index.js
@@ -99,35 +99,40 @@ var meta = {
 */
 
 /**
-  * @method toggleRead
-  * @memberOf Notification
-  * @instance
-  * @description Toggles the read state of a notification
-  * @example
-  * const InfiniteCampus = require('infinite-campus')
-  * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
-  *
-  *  // wait until we are done logging in
-  *  user.on('ready', () => {
-  *    // user is authenticated, now we can get notifications
-  *    user.getNotifications().then((notifications) => {
-  *      // if the first notification is unread
-  *      if (notification[0].read === false){
-  *        // mark it as read by toggling its read state
-  *        notifications[0].toggleRead()
-  *      }
-  *    }
-  *  })
-*/
+ * @method toggleRead
+ * @memberOf Notification
+ * @instance
+ * @description Toggles the read state of a notification
+ * @example
+ * const InfiniteCampus = require('infinite-campus')
+ * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
+ *
+ *  // wait until we are done logging in
+ *  user.on('ready', () => {
+ *    // user is authenticated, now we can get notifications
+ *    user.getNotifications().then((notifications) => {
+ *      // if the first notification is unread
+ *      if (notification[0].read === false){
+ *        // mark it as read by toggling its read state
+ *        notifications[0].toggleRead()
+ *      }
+ *    }
+ *  })
+ */
 
 /**
-  * Class representing an authenticated Infinite Campus user
-*/
+ * Class representing an authenticated Infinite Campus user
+ */
 class User extends EventEmitter {
   /**
    * Fired when the user is authenticated and ready to make API calls
    * @event User#ready
- */
+   */
+
+  /**
+   * Fired when the login process has failed
+   * @event User#error
+   */
 
   /**
     * Authenticates a user. Fires [ready]{@link User#event:ready} event when the user is logged in
@@ -147,12 +152,34 @@ class User extends EventEmitter {
 
   */
 
-  constructor (districtName, state, username, password) {
+  constructor(districtName, state, username, password) {
     super() // calls EventEmitter constructor
     this.meta = meta
     this.authenticated = false
     this.cookies = request.jar()
-    request = request.defaults({ jar: this.cookies, headers: { 'User-Agent': `GitHub ${meta.github} - v${meta.version}`, 'Accept': 'application/json' } })
+    request = request.defaults({
+      jar: this.cookies,
+      headers: {
+        'User-Agent': `GitHub ${meta.github} - v${meta.version}`,
+        'Accept': 'application/json'
+      }
+    })
+
+    // detect login errors thrown
+    // if there is a listener on 'error' event, emit it, else throw the error and kill the process
+    this.on('error', (err) => {
+      if (!this.hasErrorListener) {
+        throw err
+      }
+    })
+
+    // check to see if anybody is listening on 'error'
+    this.hasErrorListener = false
+    this.once('newListener', (event) => {
+      if (event === 'error') {
+        this.hasErrorListener = true
+      }
+    })
 
     // fetch district info & log in
     this._getDistrict(districtName, state).then(() => {
@@ -178,10 +205,14 @@ class User extends EventEmitter {
     * @private
 
   */
-  _getDistrict (districtName, state) {
+  _getDistrict(districtName, state) {
     return new Promise((resolve, reject) => {
       request('https://mobile.infinitecampus.com/mobile/searchDistrict?query=' + districtName + '&state=' + state, (err, res, body) => {
-        errHandler.district.handle(err, res, body)
+        try {
+          errHandler.district.handle(err, res, body)
+        } catch (err) {
+          return this.emit('error', err)
+        }
 
         this.district = JSON.parse(body).data[0]
         resolve()
@@ -190,21 +221,25 @@ class User extends EventEmitter {
   }
 
   /**
-    * logs a user in
-    * cookies get stored as a requestJS cookie jar in this.cookies
-    * @param {string} username - a user's username
-    * @param {string} password - a user's password
-    * @async
-    * @private
-  */
-  _login (username, password) {
+   * logs a user in
+   * cookies get stored as a requestJS cookie jar in this.cookies
+   * @param {string} username - a user's username
+   * @param {string} password - a user's password
+   * @async
+   * @private
+   */
+  _login(username, password) {
     return new Promise((resolve, reject) => {
       if (this.district === undefined) {
         throw Error('._getDistrict() must be ran before you can log in.')
       }
 
       request(this.district.district_baseurl + 'verify.jsp?nonBrowser=true&username=' + username + '&password=' + password + '&appName=' + this.district.district_app_name, (err, res, body) => {
-        errHandler.login.handle(err, res, body)
+        try {
+          errHandler.login.handle(err, res, body)
+        } catch (err) {
+          return this.emit('error', err)
+        }
 
         resolve()
       })
@@ -212,24 +247,24 @@ class User extends EventEmitter {
   }
 
   /**
-    * Fetches data for all courses a user is enrolled in. This method returns information about all terms, all courses, all grades for those courses,
-    * as well as all time placement data for those courses. See documentation for the [Term]{@link Term} and [Course]{@link Course} types for more information on what this method specifically returns. <br>
+   * Fetches data for all courses a user is enrolled in. This method returns information about all terms, all courses, all grades for those courses,
+   * as well as all time placement data for those courses. See documentation for the [Term]{@link Term} and [Course]{@link Course} types for more information on what this method specifically returns. <br>
    * @async
-    * @returns {Term[]} array of terms containing courses
-    * @example
-    * const InfiniteCampus = require('infinite-campus')
-    * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
-    *
-    * // wait until we are done logging in
-    * user.on('ready', () => {
-    *   // now that we are logged in, fetch courses
-    *   user.getCourses().then((courses) => {
-    *     console.log(courses)
-    *   })
-    * })
-    *
-  */
-  getCourses (schoolID) {
+   * @returns {Term[]} array of terms containing courses
+   * @example
+   * const InfiniteCampus = require('infinite-campus')
+   * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
+   *
+   * // wait until we are done logging in
+   * user.on('ready', () => {
+   *   // now that we are logged in, fetch courses
+   *   user.getCourses().then((courses) => {
+   *     console.log(courses)
+   *   })
+   * })
+   *
+   */
+  getCourses(schoolID) {
     return new Promise((resolve, reject) => {
       checkAuth.call(this)
       // fetch roster with placements
@@ -252,7 +287,12 @@ class User extends EventEmitter {
             // build list of schools
             let schools = []
             grades.forEach((school) => {
-              schools.push({ schoolName: school.displayName, id: school.schoolID, numberOfTerms: school.terms.length, totalNumberOfCourses: school.courses.length })
+              schools.push({
+                schoolName: school.displayName,
+                id: school.schoolID,
+                numberOfTerms: school.terms.length,
+                totalNumberOfCourses: school.courses.length
+              })
             })
 
             // throw warning is schoolID isn't specifed
@@ -273,7 +313,7 @@ class User extends EventEmitter {
             }
 
           }
-          
+
           // loop over terms from /grades
           grades[schoolIndex].terms.forEach((term, i) => {
             let termResult = {
@@ -305,17 +345,20 @@ class User extends EventEmitter {
               }
 
               // remove grades for courses without grades
-              if (!(grade.progressScore || grade.score) 
-                  && !(grade.progressPercent || grade.percent) 
-                  && !(grade.progressTotalPoints || grade.totalPoints) 
-                  && !(grade.progressPointsEarned || grade.pointsEarned)) courseResult.grades = undefined;
+              if (!(grade.progressScore || grade.score) &&
+                !(grade.progressPercent || grade.percent) &&
+                !(grade.progressTotalPoints || grade.totalPoints) &&
+                !(grade.progressPointsEarned || grade.pointsEarned)) courseResult.grades = undefined;
 
               // push class to term array
               termResult.courses.push(courseResult)
 
               // setup cross reference with a pointer into the result var
               // we can cross reference the data from both endpoints by using the class ID later
-              crossReference[course._id] = { i: i, ii: ii }
+              crossReference[course._id] = {
+                i: i,
+                ii: ii
+              }
             })
 
             // push terms to final array
@@ -362,24 +405,24 @@ class User extends EventEmitter {
   }
 
   /**
-      * gets current number of unseen notifications <br>
-      * *note: the unseen notification count is different from the read status of an individual notification*
-      * @async
-      * @returns {integer} number of unseen notifications
-      * @example
-      * const InfiniteCampus = require('infinite-campus')
-      * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
-      *
-      * // wait until we are done logging in
-      * user.on('ready', () => {
-      *   // now that we are logged in, get the notification count
-      *   user.getNotificationCount().then((count) => {
-      *     console.log(count) // returns: 7
-      *   })
-      * })
-      *
-  */
-  getNotificationCount () {
+   * gets current number of unseen notifications <br>
+   * *note: the unseen notification count is different from the read status of an individual notification*
+   * @async
+   * @returns {integer} number of unseen notifications
+   * @example
+   * const InfiniteCampus = require('infinite-campus')
+   * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
+   *
+   * // wait until we are done logging in
+   * user.on('ready', () => {
+   *   // now that we are logged in, get the notification count
+   *   user.getNotificationCount().then((count) => {
+   *     console.log(count) // returns: 7
+   *   })
+   * })
+   *
+   */
+  getNotificationCount() {
     return new Promise((resolve, reject) => {
       checkAuth.call(this)
       request(this.district.district_baseurl + 'prism?x=notifications.NotificationUser-countUnviewed', (err, res, body) => {
@@ -390,23 +433,23 @@ class User extends EventEmitter {
   }
 
   /**
-    * resets the unseen notification count. This is what happens when you click on the bell icon in Infinite Campus <br>
-    * *note: this is different from the read status of individual notifications, see [User.markAllNotificationsRead()]{@link User#markAllNotificationsRead} and [Notification.toggleRead()]{@link Notification#toggleRead}*
-    * @async
-    * @example
-    * const InfiniteCampus = require('infinite-campus')
-    * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
-    *
-    * // wait until we are done logging in
-    * user.on('ready', () => {
-    *   // now that we are logged in, reset the notification count
-    *   user.resetNotificationCount().then(() => {
-    *     // ...
-    *   })
-    * })
-    *
-  */
-  resetNotificationCount () {
+   * resets the unseen notification count. This is what happens when you click on the bell icon in Infinite Campus <br>
+   * *note: this is different from the read status of individual notifications, see [User.markAllNotificationsRead()]{@link User#markAllNotificationsRead} and [Notification.toggleRead()]{@link Notification#toggleRead}*
+   * @async
+   * @example
+   * const InfiniteCampus = require('infinite-campus')
+   * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
+   *
+   * // wait until we are done logging in
+   * user.on('ready', () => {
+   *   // now that we are logged in, reset the notification count
+   *   user.resetNotificationCount().then(() => {
+   *     // ...
+   *   })
+   * })
+   *
+   */
+  resetNotificationCount() {
     return new Promise((resolve, reject) => {
       checkAuth.call(this)
       request(this.district.district_baseurl + 'prism?x=notifications.NotificationUser-updateLastViewed', (err, res, body) => {
@@ -417,23 +460,23 @@ class User extends EventEmitter {
   }
 
   /**
-    * marks all notifications as read <br>
-    * *note: this is different from the unread count, see [User.getNotificationCount()]{@link User#getNotificationCount} and [User.resetNotificationCount()]{@link User#resetNotificationCount}*
-    * @async
-    * @example
-    * const InfiniteCampus = require('infinite-campus')
-    * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
-    *
-    * // wait until we are done logging in
-    * user.on('ready', () => {
-    *   // now that we are logged in, mark all notifications as read
-    *   user.markAllNotificationsRead().then(() => {
-    *     // ...
-    *   })
-    * })
-    *
-  */
-  markAllNotificationsRead () {
+   * marks all notifications as read <br>
+   * *note: this is different from the unread count, see [User.getNotificationCount()]{@link User#getNotificationCount} and [User.resetNotificationCount()]{@link User#resetNotificationCount}*
+   * @async
+   * @example
+   * const InfiniteCampus = require('infinite-campus')
+   * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
+   *
+   * // wait until we are done logging in
+   * user.on('ready', () => {
+   *   // now that we are logged in, mark all notifications as read
+   *   user.markAllNotificationsRead().then(() => {
+   *     // ...
+   *   })
+   * })
+   *
+   */
+  markAllNotificationsRead() {
     return new Promise((resolve, reject) => {
       checkAuth.call(this)
       request(this.district.district_baseurl + 'prism?x=notifications.Notification-markAllRead', (err, res, body) => {
@@ -444,13 +487,13 @@ class User extends EventEmitter {
   }
 
   /**
-    * toggles a given notifications read state
-    * *note: this is different from the unread count, see [User.getNotificationCount()]{@link User#getNotificationCount} and [User.resetNotificationCount()]{@link User#resetNotificationCount}* <br>
-    * this function is included in {@link Notification}. For readability this function is private and a dock
-    * @async
-    * @private
-  */
-  _toggleNotificationRead (id) {
+   * toggles a given notifications read state
+   * *note: this is different from the unread count, see [User.getNotificationCount()]{@link User#getNotificationCount} and [User.resetNotificationCount()]{@link User#resetNotificationCount}* <br>
+   * this function is included in {@link Notification}. For readability this function is private and a dock
+   * @async
+   * @private
+   */
+  _toggleNotificationRead(id) {
     return new Promise((resolve, reject) => {
       checkAuth.call(this)
       request(this.district.district_baseurl + 'prism?x=notifications.Notification-toggleRead&notificationID=' + id, (err, res, body) => {
@@ -461,32 +504,32 @@ class User extends EventEmitter {
   }
 
   /**
-    * Returns a list of notifications. <br>
-    * ###### notification types: <br>
-    * keep in mind mind that that this data may not be 100% accurate, more research needs to be done <br>
-    * * 2 - attendance updated (eg. you get marked tardy) <br>
-    * * 3 - course grade updated (eg. your class grade changes after an assignment is graded) <br>
-    * * 4 - assignment updated (eg. an assignment is graded or its state is updated as missing, late, etc )
-    * @async
-    * @param {integer} [limit=200] - number of notifications to return
-    * @returns {Notification[]} array of notifications
-    * @example
-    * // this example will mark all notifications before certain date as read
-    *
-    * // first we log in
-    * const InfiniteCampus = require('infinite-campus')
-    * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
-    *
-    * // wait until we are done logging in
-    * user.on('ready', () => {
-    *   // now that we are logged in, get notifications
-    *   user.getNotifications().then((notifications) => {
-    *     console.log(notifications)
-    *   })
-    * })
-    *
-  */
-  getNotifications (limit) {
+   * Returns a list of notifications. <br>
+   * ###### notification types: <br>
+   * keep in mind mind that that this data may not be 100% accurate, more research needs to be done <br>
+   * * 2 - attendance updated (eg. you get marked tardy) <br>
+   * * 3 - course grade updated (eg. your class grade changes after an assignment is graded) <br>
+   * * 4 - assignment updated (eg. an assignment is graded or its state is updated as missing, late, etc )
+   * @async
+   * @param {integer} [limit=200] - number of notifications to return
+   * @returns {Notification[]} array of notifications
+   * @example
+   * // this example will mark all notifications before certain date as read
+   *
+   * // first we log in
+   * const InfiniteCampus = require('infinite-campus')
+   * const user = new InfiniteCampus('New York District', 'NY', 'JDoe12', 'XXXXXX')
+   *
+   * // wait until we are done logging in
+   * user.on('ready', () => {
+   *   // now that we are logged in, get notifications
+   *   user.getNotifications().then((notifications) => {
+   *     console.log(notifications)
+   *   })
+   * })
+   *
+   */
+  getNotifications(limit) {
     return new Promise((resolve, reject) => {
       checkAuth.call(this)
 
@@ -512,7 +555,9 @@ class User extends EventEmitter {
             type: notification.notificationTypeID,
             link: this.district.district_baseurl + notification.linkUrl + notification.linkContext,
             read: (notification.read === 'true'),
-            toggleRead: () => { this._toggleNotificationRead(notification.notificationID) }
+            toggleRead: () => {
+              this._toggleNotificationRead(notification.notificationID)
+            }
           })
         })
 
@@ -523,10 +568,10 @@ class User extends EventEmitter {
 }
 
 /**
-  * helper function to make sure a user is authenticated before making API calls
-  * @private
-*/
-function checkAuth () {
+ * helper function to make sure a user is authenticated before making API calls
+ * @private
+ */
+function checkAuth() {
   // checks if the user has been authenticated
   if (!this.authenticated || this.district === undefined) {
     throw Error('User isn\'t authenticated yet. Please use the .on(\'ready\') event emitted by the User class. For help please see the example code on GitHub: https://github.com/' + meta.github)
